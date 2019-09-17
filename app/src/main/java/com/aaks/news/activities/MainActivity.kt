d48f214c74bf07d327acf5a4272b7aea306dd9f7
Patrick Alburtus <palburtus@webmd.net>
2019-09-16 21:50:49 -0400

    added button to start subscrib activity

--- app/src/main/java/com/aaks/news/activities/MainActivity.kt
+++ app/src/main/java/com/aaks/news/activities/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.aaks.news.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import com.aaks.news.R
+import com.aaks.news.subscriptions.SubscribeActivity
 
 class MainActivity : AppCompatActivity(){
 
     private lateinit var textMessage: TextView
+    private lateinit var buttonSubscribe: Button
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
@@ -29,10 +34,19 @@ class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         textMessage = findViewById(R.id.message)
+
+        buttonSubscribe = findViewById(R.id.button_subscribe)
+        buttonSubscribe.setOnClickListener {
+            val intent = Intent(applicationContext, SubscribeActivity::class.java)
+            startActivity(intent)
+        }
+
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 
